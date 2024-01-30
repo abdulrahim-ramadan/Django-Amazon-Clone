83d89b2accd0edfbc95f44da2dc4512959fc4693
abdulrahim Ramadan <147656627+abdulrahim-ramadan@users.noreply.github.com>
2024-01-31 00:10:28 +0100

    add brands file & image Brand

--- products/views.py
+++ products/views.py
@@ -27,5 +27,20 @@ class BrandList(ListView):
     model = Brand
 
 
-class BrandDetail(DetailView):
-    model = Brand
+class BrandDetail(ListView):
+    model = Product             # products ----)  # products
+    template_name = 'products/brand_detail.html'
+
+
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = super().get_queryset()    # all products
+        queryset = queryset.filter(brand = brand)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.get(slug=self.kwargs['slug']) 
+        return context
+
+
