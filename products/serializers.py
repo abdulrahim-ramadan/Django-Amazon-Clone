d2a8883fafb1f6f22dd7642479673c2d361f5451
abdulrahim Ramadan <147656627+abdulrahim-ramadan@users.noreply.github.com>
2024-02-18 03:56:37 +0100

    show productimages , reviews in Product API

--- products/serializers.py
+++ products/serializers.py
@@ -6,11 +6,27 @@ from .models import Product , Brand , Review , ProductImages
 class BrandSerialzer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = '__all__'        
+        fields = '__all__'  
+
+class ProductImagesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductImages
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+
 
 class ProductSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
-    
+    product_image = ProductImagesSerializer(many=True)
+    review_product = ReviewSerializer(many=True)
     class Meta:
         model = Product
         fields = '__all__'
