63840f9d97b484a1a7c2af73498eb94d742bd72c
abdulrahim Ramadan <147656627+abdulrahim-ramadan@users.noreply.github.com>
2024-03-29 05:45:18 +0100

    add orders , cart APIs & url structure

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from .views import add_to_cart ,order_list,checkout,process_payment,payment_failed,payment_success
+from .api import OrderDetailAPI , OrderListAPI , ApplyCouponAPI , CartCreateUpdateDeleteAPI , CreateOrderAPI
+
 
 urlpatterns = [
     path('', order_list),
@@ -7,8 +9,15 @@ urlpatterns = [
     path('checkout/process_payment',process_payment ),
     path('checkout/payment/success',payment_success),
     path('checkout/payment/failed',payment_failed ),
-
-
-
     path('add-to-cart', add_to_cart),
+
+
+    # api
+    path('api/<str:username>', OrderListAPI.as_view()),
+    path('api/<str:username>/<int:pk>', OrderDetailAPI.as_view()),
+    path('api/<str:username>/<int:pk>', CreateOrderAPI.as_view()),
+    path('api/<str:username>/cart', CartCreateUpdateDeleteAPI.as_view()),
+    path('api/<str:username>/apply-coupon', ApplyCouponAPI.as_view()),
+
+    
 ]
