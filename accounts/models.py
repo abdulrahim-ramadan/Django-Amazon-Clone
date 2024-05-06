763468a1d3a7f2c696205b5d391bc6c86f958e67
abdulrahim-ramadan <abdalrhim19991@gmail.com>
2024-05-06 23:56:22 +0200

    design user models

--- accounts/models.py
+++ accounts/models.py
@@ -2,14 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile/')
 
+
+PHONE_TYPE = (
+    ('Primary' , 'Primary') ,
+    ('Secondry' , 'Secondry') ,
+)
+
+
+class Phones(models.Model):
+    user = models.ForeignKey(User,related_name='user_phone',on_delete=models.CASCADE)
+    type = models.CharField(max_length=20,choices=PHONE_TYPE)
+    number = models.CharField(max_length=25)
+
+    def __str__(self):
+        return str(self.user)
 
 
 ADDRESS_TYPE = (
-    ('Home' , 'Home'),
-    ('Office' , 'Office'),
-    ('Bussines' , 'Bussines'),
-    ('Other' , 'Other'),
+    ('Home' , 'Home') ,
+    ('office' , 'Office') ,
+    ('Bussines' , 'Bussines',) ,
+    ('Other' , 'Other') ,
 )
 
 
@@ -17,7 +35,6 @@ class Address(models.Model):
     user = models.ForeignKey(User,related_name='user_address',on_delete=models.CASCADE)
     address = models.TextField(max_length=300)
     type = models.CharField(max_length=20,choices=ADDRESS_TYPE)
-
 
     def __str__(self):
         return self.address
