29c666db946158bf4524e41c659e5bd267f81865
abdulrahim-ramadan <abdalrhim19991@gmail.com>
2024-05-19 18:16:28 +0200

    add user signup with email

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
-
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 
 # Create your views here.
-from products.models import Product , Brand , Review # ----- dasboard
+from products.models import Product , Brand , Review
 from orders.models import Order
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+
+from .models import Profile
+from .forms import SignupForm
+
+from django.conf import settings
 
 
 def dashboard(request):
@@ -33,7 +38,39 @@ def dashboard(request):
     })
 
 def signup(request):
-    pass    
+    
+    # create new user 
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid() :
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            
+            
+            # make user un active 
+            user = form.save(commit=False)
+            user.is_active = False
+            
+            form.save()  # create new user 
+            
+            # send user email 
+            profile = Profile.objects.get(user__username=username)
+            code = profile.code 
 
-def user_activate(request):
+            send_mail(
+                "Activate Your Account",
+                f"Welcome {username}\nUse this code {code} to acitvate your account .",
+                settings.EMAIL_HOST_USER,
+                [email],
+                fail_silently=False,
+            )
+
+            
+            # redirect : activate 
+            return redirect(f'/accounts/{username}/activate')
+        
+    else:
+        form = SignupForm() 
+
+def user_activate(request,username):
     pass
