48f135bdd028a972b5ff6a628f7d811b98479867
abdulrahim-ramadan <abdalrhim19991@gmail.com>
2024-05-21 22:14:17 +0200

    add translation settings #

--- project/settings.py
+++ project/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',          # ---------> language 
     #'django.contrib.auth.backends.ModelBackend',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,13 +142,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'de'       # ------> language 
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
+
+LOCALE_PATHS = ['locale']
 
 
 # Static files (CSS, JavaScript, Images)
